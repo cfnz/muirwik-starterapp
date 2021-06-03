aef39eeb4fa888dda83d83a5a7c264952f890c0a
cf <reg@ccfraser.com>
2021-06-03 14:38:17 +1200

    Bumped versions incl to Kotlin 1.5.0. Now using mavenCentral

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,23 @@
 group = "com.ccfraser.muirwik"
-version = "0.6.3"
+version = "0.7.0"
 description = "Starter Application for Muirwik (a Material UI React wrapper written in Kotlin)"
 
 plugins {
-    kotlin("js") version "1.4.20"
+    kotlin("js") version "1.5.0"
 }
 
 repositories {
-    jcenter()
-    maven { setUrl("https://dl.bintray.com/kotlin/kotlin-js-wrappers") }
-//    mavenLocal()
+    mavenLocal()
+    mavenCentral()
 }
 
 dependencies {
-    val kotlinVersion = "1.4.20"
-    val muirwikComponentVersion = "0.6.3"
-    val kotlinJsVersion = "pre.129-kotlin-$kotlinVersion"
+    val kotlinVersion = "1.5.0"
+    val muirwikComponentVersion = "0.7.0"
+    val kotlinJsVersion = "pre.204-kotlin-$kotlinVersion"
 
     implementation(kotlin("stdlib-js", kotlinVersion))
-    implementation("org.jetbrains", "kotlin-styled", "5.2.0-$kotlinJsVersion")
+    implementation("org.jetbrains.kotlin-wrappers", "kotlin-styled", "5.3.0-$kotlinJsVersion")
     implementation(npm("react-hot-loader", "^4.12.20"))
     implementation("com.ccfraser.muirwik:muirwik-components:$muirwikComponentVersion")
 }
